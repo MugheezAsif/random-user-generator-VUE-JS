7ac00d8ae5ca20fc7ecb5dd0884f5df6290d3db7
Mugheez Asif <m.mogheesasif@gmail.com>
2022-12-29 22:50:46 +0500

    project completed

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 const app = Vue.createApp({
-    template: "<h1>Hello World</h1>",
+    data() {
+        return {
+            firstName: "Jon",
+            lastName: "Doe",
+            email: "jd@gmail.com",
+            gender: "male",
+            picture: "https://randomuser.me/api/portraits/men/10.jpg",
+        }
+    },
+    methods: {
+        async getUser(){
+            const res = await fetch('https://randomuser.me/api')
+            const result = await res.json()
+            
+            this.firstName = result[0].name.first
+            this.lastName = result[0].name.last
+            this.email = result[0].email
+            this.gender = result[0].gender
+            this.picture = result[0].picture.large
+        }
+    },
 })
 
 app.mount('#app')
